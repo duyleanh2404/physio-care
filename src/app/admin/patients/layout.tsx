6aff768e955f23ce0143e1d83e5c1171923d4313
Le Anh Duy <duyleanh2404@gmail.com>
2025-10-01 14:24:17 +0700

    FEAT: Implement manage patients (list, filter, CRUD)

--- src/app/admin/patients/layout.tsx
+++ src/app/admin/patients/layout.tsx
@@ -1,7 +1,7 @@
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Quản lý bệnh nhân",
+  title: "Quản lý người dùng",
 };
 
 export default function Layout({ children }: { children: React.ReactNode }) {
