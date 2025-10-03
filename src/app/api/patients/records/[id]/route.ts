4290821cf0cdcbd0ad1289cbe3c5f1f63c584f34
Le Anh Duy <duyleanh2404@gmail.com>
2025-10-03 17:52:02 +0700

    FIX: Add missing await for params in PUT route

--- src/app/api/patients/records/[id]/route.ts
+++ src/app/api/patients/records/[id]/route.ts
@@ -4,10 +4,10 @@ import { createAxios } from "@/lib/axios-server";
 
 export async function PUT(
   req: Request,
-  { params }: { params: { id: string } },
+  { params }: { params: Promise<{ id: string }> },
 ) {
   try {
-    const { id } = params;
+    const id = (await params).id;
 
     const api = await createAxios();
 
@@ -30,10 +30,10 @@ export async function PUT(
 
 export async function DELETE(
   req: Request,
-  { params }: { params: { id: string } },
+  { params }: { params: Promise<{ id: string }> },
 ) {
   try {
-    const { id } = params;
+    const id = (await params).id;
 
     const api = await createAxios();
 
